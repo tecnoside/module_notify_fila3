a7caca45447c16daa0fd050010cd795a3fe8205b
Malebestia <malebestia@yahoo.it>
2022-03-22 12:59:47 +0100

    .

--- Models/NotifyTheme.php
+++ Models/NotifyTheme.php
@@ -5,30 +5,19 @@ declare(strict_types=1);
 namespace Modules\Notify\Models;
 
 /**
- * --
+ * --.
  */
-class NotifyTheme extends BaseModel{
-
+class NotifyTheme extends BaseModel {
     /**
      * @var array<string>
      */
-    protected $fillable=[
+    protected $fillable = [
         'id',
         'lang',
         'type',
         'subject',
         'body',
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 468f0a0 (.)
         'body_html',
         'from',
-=======
->>>>>>> 42aa20e (.)
-=======
-        'from',
->>>>>>> 8b046e1 (.)
     ];
 }
--- Services/SmsService.php
+++ Services/SmsService.php
@@ -4,24 +4,7 @@ declare(strict_types=1);
 
 namespace Modules\Notify\Services;
 
-use Exception;
-use Illuminate\Contracts\Support\Renderable;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Arr;
-use Illuminate\Support\Collection;
-use Illuminate\Support\Facades\Cache;
-use Illuminate\Support\Facades\File;
-use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\View;
 use Illuminate\Support\Str;
-use Modules\Tenant\Services\TenantService;
-use Modules\Xot\Contracts\PanelContract;
-use Modules\Xot\Services\ArtisanService;
-use Modules\Xot\Services\FileService;
-use Modules\Xot\Services\PanelService;
-use Modules\Xot\Services\RouteService;
-use Modules\Xot\Services\StubService;
-use Modules\Xot\Traits\Getter;
 
 //---------CSS------------
 
@@ -32,18 +15,11 @@ class SmsService {
     private static ?self $instance = null;
     public ?string $from;
     public string $to;
-<<<<<<< HEAD
-<<<<<<< HEAD
-    public string $driver='netfun';
-=======
-    public string $driver;
->>>>>>> 42aa20e (.)
-=======
-    public string $driver='netfun';
->>>>>>> 8b046e1 (.)
+
+    public string $driver = 'netfun';
     public ?string $body;
 
-    public array $vars=[];
+    public array $vars = [];
 
     public static function getInstance(): self {
         if (null === self::$instance) {
@@ -57,30 +33,23 @@ class SmsService {
         return static::getInstance();
     }
 
-    public function setLocalVars(array $vars):self{
-        foreach($vars as $k=>$v){
-            $this->{$k}=$v;
+    public function setLocalVars(array $vars): self {
+        foreach ($vars as $k => $v) {
+            $this->{$k} = $v;
         }
-        $this->vars=array_merge($this->vars,$vars);
+        $this->vars = array_merge($this->vars, $vars);
+
         return $this;
     }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 8b046e1 (.)
-    public function mergeVars(array $vars):self{
-        $this->vars=array_merge($this->vars,$vars);
+    public function mergeVars(array $vars): self {
+        $this->vars = array_merge($this->vars, $vars);
+
         return $this;
     }
-<<<<<<< HEAD
-=======
->>>>>>> 42aa20e (.)
-=======
->>>>>>> 8b046e1 (.)
 
-    public function send(){
-        $class='\Modules\Notify\Services\SmsEngines\\'.Str::studly($this->driver).'Engine';
+    public function send() {
+        $class = '\Modules\Notify\Services\SmsEngines\\'.Str::studly($this->driver).'Engine';
         $class::make()
             ->setLocalVars($this->vars)
             ->send();
