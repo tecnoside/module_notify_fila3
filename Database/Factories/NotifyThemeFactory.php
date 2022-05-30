103a1c27def36d7cc77c7d33ab8ffda17efc794e
marco xot <marco.sottana@gmail.com>
2022-05-30 17:57:53 +0200

    .

---
+++ Database/Factories/NotifyThemeFactory.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Notify\Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+use Modules\Notify\Models\NotifyTheme;
+
+class NotifyThemeFactory extends Factory {
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Modules\Notify\Models\NotifyTheme::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition() {
+       
+
+        return [
+            'id' => $this->faker->randomNumber,
+            'lang' => $this->faker->word,
+            'type' => $this->faker->word,
+            'subject' => $this->faker->word,
+            'body' => $this->faker->text,
+            'body_html' => $this->faker->text,
+            'from' => $this->faker->word
+        ];
+    }
+}
