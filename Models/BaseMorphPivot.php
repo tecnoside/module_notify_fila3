89120cb2c843c1edb730d5e86c9c32ccbc3335b6
marco76tv <marco.sottana@gmail.com>
2022-05-31 08:48:54 +0200

    rebase

--- Models/BaseMorphPivot.php
+++ Models/BaseMorphPivot.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php
 
 declare(strict_types=1);
@@ -62,3 +63,69 @@ abstract class BaseMorphPivot extends MorphPivot {
         'note',
     ];
 }
+=======
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Notify\Models;
+
+use Illuminate\Database\Eloquent\Relations\MorphPivot;
+use Modules\Xot\Traits\Updater;
+
+/**
+ * Class BaseMorphPivot.
+ */
+abstract class BaseMorphPivot extends MorphPivot {
+    use Updater;
+    /**
+     * Indicates whether attributes are snake cased on arrays.
+     *
+     * @see  https://laravel-news.com/6-eloquent-secrets
+     *
+     * @var bool
+     */
+    // public static $snakeAttributes = true;
+
+    protected $perPage = 30;
+
+    protected $connection = 'notify';
+
+    /**
+     * @var array
+     */
+    protected $appends = [];
+    /**
+     * @var string
+     */
+    protected $primaryKey = 'id';
+    /**
+     * @var bool
+     */
+    public $incrementing = true;
+    /**
+     * @var bool
+     */
+    public $timestamps = true;
+    // protected $attributes = ['related_type' => 'cuisine_cat'];
+    /**
+     * @var string[]
+     */
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at',
+        // 'published_at',
+    ];
+    /**
+     * @var string[]
+     */
+    protected $fillable = [
+        'id',
+        'post_id', 'post_type',
+        'related_type',
+        'user_id',
+        'note',
+    ];
+}
+>>>>>>> 9349baf (.)
--- Models/BasePivot.php
+++ Models/BasePivot.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php
 
 declare(strict_types=1);
@@ -53,3 +54,60 @@ abstract class BasePivot extends Pivot {
      */
     public $incrementing = true;
 }
+=======
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Notify\Models;
+
+use Illuminate\Database\Eloquent\Relations\Pivot;
+// //use Laravel\Scout\Searchable;
+use Modules\Xot\Traits\Updater;
+
+/**
+ * Class BasePivot.
+ */
+abstract class BasePivot extends Pivot {
+    use Updater;
+    /**
+     * Indicates whether attributes are snake cased on arrays.
+     *
+     * @see  https://laravel-news.com/6-eloquent-secrets
+     *
+     * @var bool
+     */
+    // public static $snakeAttributes = true;
+
+    protected $perPage = 30;
+
+    // use Searchable;
+
+    /**
+     * @var string
+     */
+    protected $connection = 'notify'; // this will use the specified database conneciton
+    /**
+     * @var array
+     */
+    protected $appends = [];
+    /**
+     * @var array
+     */
+    protected $casts = [];
+    /**
+     * @var string[]
+     */
+    protected $dates = ['created_at', 'updated_at'];
+    /**
+     * Undocumented variable.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id';
+    /**
+     * @var bool
+     */
+    public $incrementing = true;
+}
+>>>>>>> 9349baf (.)
