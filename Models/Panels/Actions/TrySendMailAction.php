00b556b7c90e1d262e7e01f204ec971f26340bc4
marco xot <marco.sottana@gmail.com>
2022-08-25 19:51:43 +0200

    .

--- Models/Panels/Actions/TrySendMailAction.php
+++ Models/Panels/Actions/TrySendMailAction.php
@@ -8,6 +8,7 @@ namespace Modules\Notify\Models\Panels\Actions;
 
 // -------- services --------
 
+use Illuminate\Support\Facades\Mail;
 use Modules\Notify\Services\MailService;
 use Modules\Theme\Services\ThemeService;
 use Modules\Xot\Models\Panels\Actions\XotBasePanelAction;
@@ -20,15 +21,21 @@ use Modules\Xot\Models\Panels\Actions\XotBasePanelAction;
 class TrySendMailAction extends XotBasePanelAction {
     public bool $onItem = true;
     public string $icon = '<i class="fas fa-vial"></i>TrySendMail';
-
+    
     /**
      * @return mixed
      */
     public function handle() {
+        $options = [
+            'DuoCircle' => 'duocircle',
+            'Mailtrap' => 'mailtrap',
+        ];
+
         $view = ThemeService::getView();
 
         $view_params = [
             'view' => $view,
+            'options' => $options,
         ];
 
         return view()->make($view, $view_params);
@@ -39,9 +46,9 @@ class TrySendMailAction extends XotBasePanelAction {
      */
     public function postHandle() {
         $data = request()->all();
-        dddx($data);
+        //dddx($data);
         try {
-            Mail::raw($data['body'], function ($msg) {
+            Mail::raw($data['body'], function ($msg) use ($data) {
                 $msg->to($data['to'])->subject('Test Email');
             });
         } catch (ErrorException $e) {
--- Resources/views/admin/home/acts/try_send_mail.blade.php
+++ Resources/views/admin/home/acts/try_send_mail.blade.php
@@ -20,12 +20,16 @@
                 <x-input.group type="select" name="mailer" :options="$mailer_options"/>
 =======
                 
+                <x-input.group type="text" name="subject" />
+
                 <x-input.group type="text" name="from" />
                 
                 <x-input.group type="text" name="to" />
 
                 <x-input.group type="wysiwyg" name="body" />
 >>>>>>> e3e9786 (.)
+
+                <x-input.group type="select" name="engine" :options="$options"/>
 
                 <button class="btn btn-primary" type="submit">go!</button>
 
