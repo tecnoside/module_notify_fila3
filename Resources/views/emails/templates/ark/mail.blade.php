4aa3b2a3c8104985501b05aeb7433cf38bd54e5c
marco xot <marco.sottana@gmail.com>
2022-10-11 19:28:12 +0200

    up

---
+++ Resources/views/emails/templates/ark/mail.blade.php
@@ -0,0 +1,16 @@
+@extends('notify::emails.templates.ark')
+
+@section('content')
+
+    @include('notify::emails.templates.ark.heading', [
+		'heading' => $subject,
+		'level' => 'h1'
+	])
+
+    @include('notify::emails.templates.ark.contentStart')
+        {!! $html !!}
+    @include('notify::emails.templates.ark.contentEnd')
+
+    
+
+@stop
