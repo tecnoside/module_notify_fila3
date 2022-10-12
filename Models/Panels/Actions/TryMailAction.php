64529a0285f88de6db364b3c0781e783352e3168
marco xot <marco.sottana@gmail.com>
2022-10-12 12:36:57 +0200

    up

--- Models/Panels/Actions/TryMailAction.php
+++ Models/Panels/Actions/TryMailAction.php
@@ -8,25 +8,26 @@ namespace Modules\Notify\Models\Panels\Actions;
 
 // -------- services --------
 
-use Modules\Notify\Services\SmsService;
+use Modules\Notify\Services\MailService;
 use Modules\Theme\Services\ThemeService;
 use Modules\Xot\Models\Panels\Actions\XotBasePanelAction;
 
 // -------- bases -----------
 
 /**
- * Class TestAction.
+ 
  */
-class TestSmsAction extends XotBasePanelAction {
+class TryMailAction extends XotBasePanelAction {
     public bool $onItem = true;
-    public string $icon = '<i class="fas fa-vial"></i>SMS';
+    public string $icon = '<i class="fas fa-vial"></i>Mail';
 
     /**
      * @return mixed
      */
     public function handle() {
         $drivers = [
-            'netfun',
+            'duocircle',
+            'mailtrap',
         ];
         $i = request('i');
 
@@ -40,12 +41,26 @@ class TestSmsAction extends XotBasePanelAction {
             'driver' => $driver,
         ];
 
+        // return view()->make($view, $view_params);
+        // Parameter #1 $view of function view expects view-string|null, mixed given.
+        // The custom 'view-string' type class. It's a subset of the string type. Every string that passes the
+        // view()->exists($string) test is a valid view-string type.
+
+        // if (view()->exists($view)) {
         return view()->make($view, $view_params);
+        // }
+
+        // return 'not exists ['.$view.']';
     }
 
+    /**
+     * ---.
+     */
     public function postHandle() {
         $data = request()->all();
         $vars = collect($data)->only(['driver', 'from', 'to', 'body'])->all();
-        SmsService::make()->setLocalVars($vars)->send();
+        MailService::make()
+            ->setLocalVars($vars)
+            ->try();
     }
 }
