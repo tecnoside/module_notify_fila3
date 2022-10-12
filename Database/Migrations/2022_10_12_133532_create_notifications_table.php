40ebae8f9dc6009a16bfaccacd6969d3b40521d0
marco xot <marco.sottana@gmail.com>
2022-10-12 13:41:07 +0200

    up

---
+++ Database/Migrations/2022_10_12_133532_create_notifications_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Modules\Xot\Database\Migrations\XotBaseMigration;
+
+
+class CreateNotificationsTable extends XotBaseMigration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(){
+         // -- CREATE --
+        $this->tableCreate(
+            function (Blueprint $table) {
+                $table->uuid('id')->primary();
+                $table->string('type');
+                $table->morphs('notifiable');
+                $table->text('data');
+                $table->timestamp('read_at')->nullable();
+                $table->timestamps();
+            }
+        );
+         // -- UPDATE --
+        $this->tableUpdate(
+            function (Blueprint $table) {
+                if (! $this->hasColumn('updated_at')) {
+                    $table->timestamps();
+                }
+                if (! $this->hasColumn('updated_by')) {
+                    $table->string('updated_by')->nullable()->after('updated_at');
+                    $table->string('created_by')->nullable()->after('created_at');
+                }
+            }
+        );
+    }
+
+    
+};
---
+++ Models/Notification.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Notify\Models;
+
+
+class Notification extends BaseModel {
+    
+}
