477637fe60ad0623ad15b414e69026bb042defc6
marco xot <marco.sottana@gmail.com>
2022-12-22 12:56:31 +0100

    .

--- Actions/ApplyModelAttrsToThemeAction.php
+++ Actions/ApplyModelAttrsToThemeAction.php
@@ -5,20 +5,24 @@ declare(strict_types=1);
 namespace Modules\Notify\Actions;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
 use Modules\Notify\Models\NotifyTheme;
 use Spatie\QueueableAction\QueueableAction;
 
-class ApplyModelAttrsToThemeAction {
+class ApplyModelAttrsToThemeAction
+{
     use QueueableAction;
 
-    public function __construct() {
+    public function __construct()
+    {
     }
 
     /**
      * Execute the action.
      */
-    public function execute(Model $model, string $name, array $extra = []): NotifyTheme {
-      
+    public function execute(Model $model, string $name, array $extra = [], ?Collection $options): NotifyTheme
+    {
+
         $theme = NotifyTheme::firstOrCreate(
             [
                 'lang' => app()->getLocale(),
@@ -37,23 +41,46 @@ class ApplyModelAttrsToThemeAction {
             $theme->save();
         }
 
+        if (!empty($options)) {
+            $string_to_replace = \Str::between($theme->body_html, '##foreach##', '##endforeach##');
+            $items='';
+            foreach ($options as $option) {
+                $extra = [
+                    'highlights' => collect($option->highlights)->pluck('txt')->implode('<hr/>'),
+                ];
+
+                $item = $this->replacer($option, $extra, $string_to_replace);
+                $items.= $item;
+            }
+
+            $theme->body_html = \str_replace($string_to_replace, $items, $theme->body_html);
+        }
+
         $data = $model->toArray();
 
         $data = array_merge($data, $extra);
-    
+
         $html = $theme->body_html;
+        $html = $this->replacer($model, $extra, $html);
+
+        $theme->mail_body = $html;
+
+        return $theme;
+    }
+
+    public function replacer($model, $extra, $html)
+    {
+        $data = $model->toArray();
+        $data = array_merge($data, $extra);
+
         foreach ($data as $k => $v) {
             if (\is_string($v)) {
-                $html = str_replace('##'.$k.'##', $v, $html);
+
+                $html = str_replace('##' . $k . '##', $v, $html);
             }
         }
 
         $html = preg_replace('/##\w+##/', 'N.D.', $html);
-
-        $theme->mail_body = $html;
-
-        // dddx($theme->mail_body);
-
-        return $theme;
+        return $html;
     }
 }
