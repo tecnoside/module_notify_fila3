7c2cbab09778dab6fa1fdd7e37ab8f2c5911aa7d
marco xot <marco.sottana@gmail.com>
2023-01-26 09:45:06 +0100

    .

--- Data/BeautyEmailData.php
+++ Data/BeautyEmailData.php
@@ -7,8 +7,11 @@ namespace Modules\Notify\Data;
 use Spatie\LaravelData\Data;
 use Modules\Notify\Data\BeautyEmailViewData;
 
-class BeautyEmailData extends Data {
-    public BeautyEmailViewData $view;
-    public array $css;
+class BeautyEmailData extends Data
+{
+    public array $view;
+    //css must not be accessed before its inizialization.
+    //sulla config beautymail di localhost manca, quindi va gestito se è vuoto
+    public ?array $css = [];
     public array $colors;
 }
