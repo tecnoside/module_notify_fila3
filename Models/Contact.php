dacc81735b9d19ea390e337afeaabd83e2cd42d5
marco76tv <marco.sottana@gmail.com>
2023-02-14 14:31:14 +0100

    .

--- Models/Contact.php
+++ Models/Contact.php
@@ -5,5 +5,5 @@ declare(strict_types=1);
 namespace Modules\Notify\Models;
 
 class Contact extends BaseModel {
-    protected $fillable = ['model_id', 'model_type', 'contact_type', 'value', 'verified_at', 'updated_at', 'created_at', 'updated_by', 'created_by'];
+    protected $fillable = ['model_id', 'model_type', 'contact_type', 'value', 'verified_at', 'updated_at', 'created_at', 'updated_by', 'created_by', 'user_id', 'token'];
 }
