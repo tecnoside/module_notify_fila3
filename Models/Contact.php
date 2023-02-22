91728dd68b60ee9a7a7add5004697ac1f6bd53a1
Malebestia <vair81@gmail.com>
2023-02-22 17:25:35 +0100

    up

--- Models/Contact.php
+++ Models/Contact.php
@@ -4,6 +4,15 @@ declare(strict_types=1);
 
 namespace Modules\Notify\Models;
 
+/**
+ * @property string $token
+ * @property string $model_type
+ * @property int    $model_id
+ */
 class Contact extends BaseModel {
-    protected $fillable = ['model_id', 'model_type', 'contact_type', 'value', 'verified_at', 'updated_at', 'created_at', 'updated_by', 'created_by', 'user_id', 'token'];
+    protected $fillable = [
+        'model_id', 'model_type', 'contact_type', 'value',
+        'verified_at', 'updated_at', 'created_at',
+        'updated_by', 'created_by', 'user_id', 'token',
+    ];
 }
