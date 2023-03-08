12a3630a2010bf6767a4c2d3c454b6750db06712
marco76tv <marco.sottana@gmail.com>
2023-03-08 15:45:49 +0100

    up

--- Models/Panels/NotificationPanel.php
+++ Models/Panels/NotificationPanel.php
@@ -44,7 +44,7 @@ class NotificationPanel extends XotBasePanel {
      *
      * @return RowsContract
      */
-    public static function indexQuery(array $data, $query) {
+    public function indexQuery(array $data, $query) {
         // return $query->where('user_id', $request->user()->id);
         return $query;
     }
