87dbc2d3e88a7d28458d42f0a0e430ddef74bc55
marco76tv <marco.sottana@gmail.com>
2023-03-20 12:35:06 +0100

    up

--- Services/NamirialService.php
+++ Services/NamirialService.php
@@ -333,6 +333,10 @@ class NamirialService
                 ],
             ],*/
         ];
+
+        //sbagliato. non prende il dato preferito di email
+        //dd($this);
+
         $this->request();
 
         //Debugbar::info('envelopeSend Params', $this->params);
