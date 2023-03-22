aa28fe1aba9c1a603b9a421782506c1e7c874e2a
marco xot <marco.sottana@gmail.com>
2023-03-22 16:43:22 +0100

    up

---
+++ Datas/SmsData.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Notify\Datas;
+
+use Spatie\LaravelData\Data;
+
+class SmsData extends Data {
+    public string $email;
+    // public string $mobile_phone;
+    // public string $token;
+    // public int $q;
+}
