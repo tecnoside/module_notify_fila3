d10ca58db258979632d9cfe3440ef860d34f08ad
marco76tv <marco.sottana@gmail.com>
2023-03-30 16:48:09 +0200

    up

---
+++ Actions/BuildUserMailMessageAction.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Notify\Actions;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Support\Str;
+use Modules\Notify\Models\NotifyTheme;
+use Spatie\QueueableAction\QueueableAction;
+
+class BuildMailMessageAction {
+    use QueueableAction;
+
+    public function execute(string $name, Model $model, array $view_params): MailMessage {
+        $view_params = array_merge($view_params, $model->toArray());
+
+        $theme = NotifyTheme::firstOrCreate([
+            'lang' => $view_params['lang'],
+            'type' => $view_params['via'] ?? 'email', // email,sms,whatsapp,piccione
+            'post_type' => $name,
+            'post_id' => $view_params['post_id'] ?? 0,
+        ]);
+        if (null == $theme->subject) {
+            $subject = trans('lu::auth.'.$name.'.subject');
+            $theme->update(['subject' => $subject]);
+        }
+        if (null == $theme->theme) {
+            $theme->update(['theme' => 'ark']);
+        }
+        if (null == $theme->body_html) {
+            $html = trans('lu::auth.'.$name.'.body_html');
+
+            if ('verify-email' == $name && 3 == $view_params['post_id']) {
+                $html .= '<br/>When you\'ll re-login this will be your password: ##password##';
+            }
+
+            $theme->update(['body_html' => $html]);
+        }
+        $view_params = array_merge($view_params, $theme->toArray());
+        // $this->view_params['url'] = (string)$url;
+
+        $body_html = $theme->body_html;
+        foreach ($view_params as $k => $v) {
+            if (is_string($v)) {
+                $body_html = Str::replace('##'.$k.'##', $v, $body_html);
+            }
+        }
+
+        $view_params['body_html'] = $body_html;
+
+        $view_html = 'lu::auth.emails.html';
+
+        // $out = view($view_html, $this->view_params);
+        // dddx($this->view_params);
+        // die($out->render());
+
+        return (new MailMessage())
+            // ->from('barrett@example.com', 'Barrett Blair')
+            ->subject($theme->subject)
+            ->view($view_html, $view_params);
+    }
+}
