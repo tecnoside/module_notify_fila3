74414e5e12055e4a3b50733c7dd234c8bac909fc
marco76tv <marco.sottana@gmail.com>
2023-03-30 16:06:06 +0200

    up

--- Models/Panels/NotifyThemePanel.php
+++ Models/Panels/NotifyThemePanel.php
@@ -422,7 +422,7 @@ class NotifyThemePanel extends XotBasePanel {
 
             (object) [
                 'type' => 'UploadImage',
-                'name' => 'logo_path',
+                'name' => 'logo_src',
                 'col_size' => 3,
                 'comment' => null,
             ],
