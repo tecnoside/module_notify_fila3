2d603ec33746898328dd513234e1ba19339a5fb3
marco xot <marco.sottana@gmail.com>
2023-04-20 17:15:21 +0200

    .

--- Notifications/RowAttributeNotification.php
+++ Notifications/RowAttributeNotification.php
@@ -8,10 +8,6 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
-<<<<<<< HEAD
-=======
-use Modules\Notify\Datas\EmailData;
->>>>>>> 5b4ef66 (up)
 use Modules\Notify\Datas\SmsData;
 use Modules\Xot\Contracts\ModelContactContract;
 
@@ -51,7 +47,6 @@ class RowAttributeNotification extends Notification implements ShouldQueue
      */
     public function toMail($notifiable)
     {
-<<<<<<< HEAD
         // Access to an undefined property Modules\Xot\Contracts\ModelContactContract::$notifyThemes.
         // $theme = $this->row->notifyThemes->first()->theme ?? 'ark';
 
@@ -61,28 +56,6 @@ class RowAttributeNotification extends Notification implements ShouldQueue
         if (! property_exists($this->row, 'mail_from_email')) {
             throw new \Exception('['.__LINE__.']['.__FILE__.']');
         }
-=======
-        $theme = $this->row->notifyThemes->first()->theme ?? 'ark';
-        // dddx($theme);
-        // dddx([
-        //     'row' => $this->row,
-        //     'from_email' => $this->row->mail_from,
-        //     'from' => $this->row->mail_from_email,
-        //     'subject' => $this->row->mail_subject,
-        //     'row email' => $this->row->email,
-        //     'body' => $this->row->mail_body,
-        //     // 'body_html' => $this->row->
-        // ]);
-
-        // $email_data = EmailData::from([
-        //     'from' =>$this->row->mail_from_email,
-        //     'from_email' => $this->row->mail_from,
-        //     'subject' => $this->row->mail_subject,
-        //     'body_html' => $theme->body_html,
-        //     'body' => $this->row->mail_body,
-        // ]);
-        // dddx($email_data);
->>>>>>> 5b4ef66 (up)
 
         $message = (new MailMessage())
             ->from($this->row->mail_from_email, $this->row->mail_from)
