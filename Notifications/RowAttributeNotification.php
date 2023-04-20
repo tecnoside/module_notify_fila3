0ae25a8d3ada955a5d341e83f47ab6b9f67443d9
marco xot <marco.sottana@gmail.com>
2023-04-20 17:09:10 +0200

    .

--- Notifications/RowAttributeNotification.php
+++ Notifications/RowAttributeNotification.php
@@ -5,12 +5,17 @@ declare(strict_types=1);
 namespace Modules\Notify\Notifications;
 
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
+<<<<<<< HEAD
+=======
+use Modules\Notify\Datas\EmailData;
+>>>>>>> 5b4ef66 (up)
 use Modules\Notify\Datas\SmsData;
 use Modules\Xot\Contracts\ModelContactContract;
 
-class RowAttributeNotification extends Notification
+class RowAttributeNotification extends Notification implements ShouldQueue
 {
     use Queueable;
     public ModelContactContract $row;
@@ -46,6 +51,7 @@ class RowAttributeNotification extends Notification
      */
     public function toMail($notifiable)
     {
+<<<<<<< HEAD
         // Access to an undefined property Modules\Xot\Contracts\ModelContactContract::$notifyThemes.
         // $theme = $this->row->notifyThemes->first()->theme ?? 'ark';
 
@@ -55,6 +61,28 @@ class RowAttributeNotification extends Notification
         if (! property_exists($this->row, 'mail_from_email')) {
             throw new \Exception('['.__LINE__.']['.__FILE__.']');
         }
+=======
+        $theme = $this->row->notifyThemes->first()->theme ?? 'ark';
+        // dddx($theme);
+        // dddx([
+        //     'row' => $this->row,
+        //     'from_email' => $this->row->mail_from,
+        //     'from' => $this->row->mail_from_email,
+        //     'subject' => $this->row->mail_subject,
+        //     'row email' => $this->row->email,
+        //     'body' => $this->row->mail_body,
+        //     // 'body_html' => $this->row->
+        // ]);
+
+        // $email_data = EmailData::from([
+        //     'from' =>$this->row->mail_from_email,
+        //     'from_email' => $this->row->mail_from,
+        //     'subject' => $this->row->mail_subject,
+        //     'body_html' => $theme->body_html,
+        //     'body' => $this->row->mail_body,
+        // ]);
+        // dddx($email_data);
+>>>>>>> 5b4ef66 (up)
 
         $message = (new MailMessage())
             ->from($this->row->mail_from_email, $this->row->mail_from)
