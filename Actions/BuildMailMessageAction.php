b068ab90d653cf6e9635ad2bbf507b9e7a79f09c
marco76tv <marco.sottana@gmail.com>
2023-05-26 14:43:44 +0200

    up

--- Actions/BuildMailMessageAction.php
+++ Actions/BuildMailMessageAction.php
@@ -14,7 +14,7 @@ class BuildMailMessageAction
 {
     use QueueableAction;
 
-    public function execute(string $name, Model $model, array $view_params = []): MailMessage
+    public function execute(string $name, Model $model, array $view_params = [], ?array $attachments = []): MailMessage
     {
         $view_params = array_merge($view_params, $model->toArray());
         $type = 'email';
@@ -71,11 +71,17 @@ class BuildMailMessageAction
 
         $view_html = 'notify::email';
 
-        return (new MailMessage())
-            // ->from('barrett@example.com', 'Barrett Blair')
+        $email = (new MailMessage())
             ->subject($view_params['subject'] ?? $theme->subject)
-            ->view($view_html, $view_params)
-            // TO-DO: va messo qua il ciclo degli attachments?
-        ;
+            ->view($view_html, $view_params);
+
+        // TO-DO: va messo qua il ciclo degli attachments?
+        if (! empty($attachments)) {
+            foreach ($attachments as $attachment) {
+                $email = $email->attach($attachment['file_path'], ['as' => $attachment['as'], 'mime' => $attachment['mime']]);
+            }
+        }
+
+        return $email;
     }
 }
