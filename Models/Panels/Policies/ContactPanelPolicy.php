83eecb98f4e66eec8683961843cc7d6ad18145ac
marco76tv <marco.sottana@gmail.com>
2023-06-27 11:32:19 +0200

    up

--- Models/Panels/Policies/ContactPanelPolicy.php
+++ Models/Panels/Policies/ContactPanelPolicy.php
@@ -2,7 +2,7 @@
 namespace Modules\Notify\Models\Panels\Policies;
 
 use Illuminate\Auth\Access\HandlesAuthorization;
-use Modules\LU\Models\User as User;
+use Modules\User\Models\User as User;
 use Modules\Notify\Models\Panels\Policies\ContactPanelPolicy as Post; 
 
 use Modules\Cms\Models\Panels\Policies\XotBasePanelPolicy;
