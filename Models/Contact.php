a027d98d8b7dce842ccb0b61465ff41f6063bda0
Malebestia <vair81@gmail.com>
2023-09-04 13:16:52 +0200

    rebase

--- Models/Contact.php
+++ Models/Contact.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Modules\Notify\Models;
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 /**
  * Modules\Notify\Models\Contact.
  *
@@ -132,4 +133,17 @@ class Contact extends BaseModel {
 =======
     protected $fillable = ['model_id', 'model_type', 'contact_type', 'value', 'verified_at', 'updated_at', 'created_at', 'updated_by', 'created_by', 'user_id', 'token'];
 >>>>>>> 32428ef (.)
+=======
+/**
+ * @property string $token
+ * @property string $model_type
+ * @property int    $model_id
+ */
+class Contact extends BaseModel {
+    protected $fillable = [
+        'model_id', 'model_type', 'contact_type', 'value',
+        'verified_at', 'updated_at', 'created_at',
+        'updated_by', 'created_by', 'user_id', 'token',
+    ];
+>>>>>>> 91728dd (up)
 }
