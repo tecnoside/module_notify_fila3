a062feb66551c085a01c1aef04ac8f413594d59d
Retype GitHub Action <retype@object.net>
2023-09-04 11:31:04 +0000

    Refreshes Retype-generated documentation.
    Process triggered by marco76tv.

--- filament/resources/notifythemeresource.php
+++ filament/resources/notifythemeresource.php
@@ -89,9 +89,23 @@ class NotifyThemeResource extends Resource
                 Forms\Components\TextInput::make('logo_src'),
                 Forms\Components\TextInput::make('logo_width'),
                 Forms\Components\TextInput::make('logo_height'),
+<<<<<<< HEAD
                 Forms\Components\TextInput::make('theme'),
 
 >>>>>>> 9658d21 (up)
+=======
+                // Forms\Components\TextInput::make('theme'),
+                Forms\Components\Select::make('theme')->options(
+                    [
+                        'empty' => 'empty',
+                        'ark' => 'ark',
+                        'minty' => 'minty',
+                        'sunny' => 'sunny',
+                        'widgets' => 'widgets',
+                    ]
+                )
+                ->default('empty'),
+>>>>>>> e4aae54 (.)
                 Forms\Components\RichEditor::make('body')->columnSpanFull(),
                 Forms\Components\RichEditor::make('body_html')->columnSpanFull(),
             ]);
