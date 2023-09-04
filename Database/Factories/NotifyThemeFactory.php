de769ef5c34cd35b2ec4ca3df9fc390f301777a9
Malebestia <vair81@gmail.com>
2023-09-04 13:13:06 +0200

    rebase

--- Database/Factories/NotifyThemeFactory.php
+++ Database/Factories/NotifyThemeFactory.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Modules\Notify\Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+<<<<<<< HEAD
 
 class NotifyThemeFactory extends Factory
 {
@@ -14,22 +15,47 @@ class NotifyThemeFactory extends Factory
      * @var class-string<\Illuminate\Database\Eloquent\Model>
      */
     protected $model = \Modules\Notify\Models\NotifyTheme::class;
+=======
+use Illuminate\Support\Str;
+
+use Modules\Notify\Models\NotifyTheme;
+
+class NotifyThemeFactory extends Factory {
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Modules\Notify\Models\NotifyTheme::class;
+>>>>>>> 103a1c2 (.)
 
     /**
      * Define the model's default state.
      *
      * @return array
      */
+<<<<<<< HEAD
     public function definition()
     {
         return [
             'id' => $this->faker->randomNumber(5, false),
+=======
+    public function definition() {
+       
+
+        return [
+            'id' => $this->faker->randomNumber,
+>>>>>>> 103a1c2 (.)
             'lang' => $this->faker->word,
             'type' => $this->faker->word,
             'subject' => $this->faker->word,
             'body' => $this->faker->text,
             'body_html' => $this->faker->text,
+<<<<<<< HEAD
             'from' => $this->faker->word,
+=======
+            'from' => $this->faker->word
+>>>>>>> 103a1c2 (.)
         ];
     }
 }
