bc07b610da083e091cdefcd86ac4c03450d05cc9
Malebestia <vair81@gmail.com>
2023-09-04 13:19:47 +0200

    rebase

--- Http/Middleware/FilamentMiddleware.php
+++ Http/Middleware/FilamentMiddleware.php
@@ -1,5 +1,6 @@
 <?php
 
+<<<<<<< HEAD
 declare(strict_types=1);
 
 namespace Modules\Notify\Http\Middleware;
@@ -10,4 +11,61 @@ class FilamentMiddleware extends XotBaseFilamentMiddleware
 {
     public static string $module = 'Notify';
     public static string $context = 'filament';
+=======
+namespace Modules\Notify\Http\Middleware;
+
+use Filament\Models\Contracts\FilamentUser;
+use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Nwidart\Modules\Laravel\Module;
+
+class FilamentMiddleware extends Middleware
+{
+    public static string $module = 'Notify';
+    public static string $context = 'filament';
+
+    private function getModule(): Module {
+        return app('modules')->findOrFail(static::$module);
+    }
+
+    /**
+     * @throws \Exception
+     */
+    private function getContextName(): string {
+        $module = $this->getModule();
+        if (!static::$context) {
+            throw new \Exception("Context has to be defined in your class");
+        }
+        return \Str::of($module->getLowerName())->append('-')->append(\Str::slug(static::$context))->kebab()->toString();
+    }
+    protected function authenticate($request, array $guards): void
+    {
+        $context = $this->getContextName();
+        $guardName = config("$context.auth.guard");
+        $guard = $this->auth->guard($guardName);
+
+        if (!$guard->check()) {
+            $this->unauthenticated($request, $guards);
+
+            return;
+        }
+
+        $this->auth->shouldUse($guardName);
+
+        $user = $guard->user();
+
+        if ($user instanceof FilamentUser) {
+            abort_if(!$user->canAccessFilament(), 403);
+
+            return;
+        }
+
+        abort_if(config('app.env') !== 'local', 403);
+    }
+
+    protected function redirectTo($request): string
+    {
+        $context = $this->getContextName();
+        return route("$context.auth.login");
+    }
+>>>>>>> 911eb41 (up)
 }
