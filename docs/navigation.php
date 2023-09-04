d2c0946d4eb22cec6507cac80315a7dca60a274a
Retype GitHub Action <retype@object.net>
2023-09-04 11:37:26 +0000

    Refreshes Retype-generated documentation.
    Process triggered by marco76tv.

--- docs/navigation.php
+++ docs/navigation.php
@@ -22,6 +22,7 @@ return [
     // ],
     // 'Jigsaw Docs' => 'https://jigsaw.tighten.co/docs/installation',
 <<<<<<< HEAD
+<<<<<<< HEAD
     'Funzione del Modulo' => [
         'url' => 'docs/module_notify',
         'children' => [
@@ -122,11 +123,16 @@ return [
 =======
 =======
 >>>>>>> a758bd5 (.)
+=======
+>>>>>>> b12d3b6 (.)
     'Funzione del Modulo' => [
         'url' => 'docs/module_notify',
         'children' => [
-            'TrySendMailAction' => 'docs/try_send_mail_action',
+            'Notifica ThemeNotification' => 'docs/theme-notification',
         ],
+    ],
+    'TrySendMailAction' => [
+        'url' => 'docs/try_send_mail_action',
     ],
 <<<<<<< HEAD
 >>>>>>> 8f4c37e (.)
