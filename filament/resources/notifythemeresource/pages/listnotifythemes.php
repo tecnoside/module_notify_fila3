549ccc861396142bd0125427e8f6fd276c1fe539
Retype GitHub Action <retype@object.net>
2023-09-04 11:38:35 +0000

    Refreshes Retype-generated documentation.
    Process triggered by marco76tv.

--- filament/resources/notifythemeresource/pages/listnotifythemes.php
+++ filament/resources/notifythemeresource/pages/listnotifythemes.php
@@ -2,6 +2,7 @@
 
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 >>>>>>> b735fbf (.)
 declare(strict_types=1);
@@ -21,6 +22,13 @@ use Filament\Resources\Pages\ListRecords;
 =======
 use Modules\Notify\Filament\Resources\NotifyThemeResource;
 >>>>>>> 42f9249 (.)
+=======
+namespace Modules\Notify\Filament\Resources\NotifyThemeResource\Pages;
+
+use Modules\Notify\Filament\Resources\NotifyThemeResource;
+use Filament\Pages\Actions;
+use Filament\Resources\Pages\ListRecords;
+>>>>>>> 01564cc (up)
 
 class ListNotifyThemes extends ListRecords
 {
