712731cd66f7887e45812d26babbfed064961a7e
Malebestia <vair81@gmail.com>
2023-09-04 13:20:46 +0200

    rebase

--- Actions/EsendexSendAction.php
+++ Actions/EsendexSendAction.php
@@ -7,6 +7,7 @@ namespace Modules\Notify\Actions;
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 use Exception;
 use Modules\Notify\Datas\SmsData;
 use Spatie\QueueableAction\QueueableAction;
@@ -123,10 +124,18 @@ use Spatie\QueueableAction\QueueableAction;
 
 use function Safe\curl_init;
 use function Safe\curl_setopt;
+=======
+>>>>>>> 78ec921 (up)
 use function Safe\curl_exec;
-use function Safe\curl_getinfo;
+use function Safe\curl_init;
+
+use Webmozart\Assert\Assert;
+use function Safe\curl_setopt;
 use function Safe\json_decode;
 use function Safe\json_encode;
+use function Safe\curl_getinfo;
+use Modules\Notify\Datas\SmsData;
+use Spatie\QueueableAction\QueueableAction;
 
 /**
  * @property string $base_endpoint
@@ -193,7 +202,7 @@ class EsendexSendAction
         $response = curl_exec($ch);
         $info = curl_getinfo($ch);
         curl_close($ch);
-
+        Assert::isArray($info);
         if (201 != $info['http_code']) {
             return [];
         }
