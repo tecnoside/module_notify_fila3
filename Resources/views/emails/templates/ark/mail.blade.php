d100add3e7a64c5038996bcbfadda67ae82d07c7
Malebestia <vair81@gmail.com>
2023-09-04 13:23:23 +0200

    rebase

--- Resources/views/emails/templates/ark/mail.blade.php
+++ Resources/views/emails/templates/ark/mail.blade.php
@@ -5,6 +5,7 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     {{--
 =======
 >>>>>>> 3405de3 (up)
@@ -14,6 +15,8 @@
 =======
     {{--
 >>>>>>> fe06862 (.)
+=======
+>>>>>>> 4aa3b2a (up)
     @include('notify::emails.templates.ark.heading', [
 		'heading' => $subject,
 		'level' => 'h1'
@@ -21,6 +24,7 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     --}}
 =======
 >>>>>>> 3405de3 (up)
@@ -30,11 +34,14 @@
 =======
     --}}
 >>>>>>> fe06862 (.)
+=======
+>>>>>>> 4aa3b2a (up)
 
     @include('notify::emails.templates.ark.contentStart')
         {!! $html !!}
     @include('notify::emails.templates.ark.contentEnd')
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -48,5 +55,8 @@
 =======
 
 >>>>>>> fe06862 (.)
+=======
+    
+>>>>>>> 4aa3b2a (up)
 
 @stop
