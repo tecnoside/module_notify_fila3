7399cf0ff34501a22664c885e7422608a9012bed
Malebestia <vair81@gmail.com>
2023-09-04 13:14:58 +0200

    rebase

---
+++ Notifications/HtmlNotification.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Notify\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class HtmlNotification extends Notification {
+    use Queueable;
+    public string $subject;
+    public string $html;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(string $subject, string $html) {
+        $this->subject = $subject;
+        $this->html = $html;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param mixed $notifiable
+     *
+     * @return array
+     */
+    public function via($notifiable) {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param mixed $notifiable
+     *
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable) {
+        return (new MailMessage())
+            ->subject($this->subject)
+            ->line('---')
+            ->view('notify::notifications.html', ['html' => $this->html]);
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param mixed $notifiable
+     *
+     * @return array
+     */
+    public function toArray($notifiable) {
+        dddx($notifiable);
+
+        return [
+        ];
+    }
+}
