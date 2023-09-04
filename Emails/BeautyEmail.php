72fdd9aa83fa28950b079e22ce2bab96ba8241d2
Malebestia <vair81@gmail.com>
2023-09-04 13:15:33 +0200

    rebase

--- Emails/BeautyEmail.php
+++ Emails/BeautyEmail.php
@@ -28,12 +28,12 @@ class BeautyEmail implements Mailer {
      * Initialise the settings and mailer.
      */
     public function __construct(?array $settings = null) {
-<<<<<<< HEAD
         if (null == $settings) {
             $settings = array_merge(
                 config('beautymail.view'),
                 [
                     'css' => ! is_null(config('beautymail.css')) && count(config('beautymail.css')) > 0 ? implode(' ', config('beautymail.css')) : '',
+<<<<<<< HEAD
 =======
         if (null === $settings) {
             $settings = array_merge(
@@ -41,6 +41,8 @@ class BeautyEmail implements Mailer {
                 [
                     'css' => null !== config('beautymail.css') && \count(config('beautymail.css')) > 0 ? implode(' ', config('beautymail.css')) : '',
 >>>>>>> 602db11 (up)
+=======
+>>>>>>> 915cbe1 (.)
                 ]
             );
         }
@@ -110,8 +112,11 @@ class BeautyEmail implements Mailer {
     }
 
     /**
+<<<<<<< HEAD
      * @param mixed $view
      *
+=======
+>>>>>>> 915cbe1 (.)
      * @return \Illuminate\View\View
      */
     public function view($view, array $data = []) {
