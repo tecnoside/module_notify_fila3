2abcc9788d10159af2f4d6a6707771d269702310
Malebestia <vair81@gmail.com>
2023-09-04 13:15:04 +0200

    rebase

--- Resources/views/emails/templates/ark/mail.blade.php
+++ Resources/views/emails/templates/ark/mail.blade.php
@@ -2,17 +2,27 @@
 
 @section('content')
 
+<<<<<<< HEAD
     {{--
+=======
+>>>>>>> 3405de3 (up)
     @include('notify::emails.templates.ark.heading', [
 		'heading' => $subject,
 		'level' => 'h1'
 	])
+<<<<<<< HEAD
     --}}
+=======
+>>>>>>> 3405de3 (up)
 
     @include('notify::emails.templates.ark.contentStart')
         {!! $html !!}
     @include('notify::emails.templates.ark.contentEnd')
 
+<<<<<<< HEAD
 
+=======
+    
+>>>>>>> 3405de3 (up)
 
 @stop
