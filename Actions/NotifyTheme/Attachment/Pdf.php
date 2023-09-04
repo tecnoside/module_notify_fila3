bba52f1ca64ae27a188527effaafb43098fafd13
Malebestia <vair81@gmail.com>
2023-09-04 13:19:21 +0200

    rebase

--- Actions/NotifyTheme/Attachment/Pdf.php
+++ Actions/NotifyTheme/Attachment/Pdf.php
@@ -5,13 +5,19 @@ declare(strict_types=1);
 namespace Modules\Notify\Actions\NotifyTheme\Attachment;
 
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 6eafc0a (up)
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 use Modules\Notify\Datas\AttachmentData;
 use Modules\Xot\Services\HtmlService;
+<<<<<<< HEAD
 =======
 use Modules\Notify\Models\NotifyTheme;
 >>>>>>> 4a7ff36 (up)
+=======
+>>>>>>> 6eafc0a (up)
 use Spatie\QueueableAction\QueueableAction;
 
 /**
@@ -21,6 +27,7 @@ class Pdf
 {
     use QueueableAction;
 
+<<<<<<< HEAD
 <<<<<<< HEAD
     public function execute(string $post_type, array $view_params): AttachmentData
     {
@@ -49,6 +56,25 @@ class Pdf
             'post_type' => $post_type,
             'post_id' => 0,
 >>>>>>> 4a7ff36 (up)
+=======
+    public function execute(string $post_type, array $view_params): AttachmentData
+    {
+        $notify_theme_data = app(\Modules\Notify\Actions\NotifyTheme\Get::class)->execute($post_type, 'pdf', $view_params);
+        $html = $notify_theme_data->body_html;
+
+        $file_name = Str::slug($notify_theme_data->subject).'.pdf';
+        if (isset($view_params[$file_name])) {
+            $file_name = $view_params[$file_name];
+        }
+        $file_path = Storage::disk('cache')->path($file_name);
+
+        HtmlService::toPdf(['filename' => $file_path, 'html' => $html, 'out' => 'file', 'pdforientation' => 'P']);
+
+        return AttachmentData::from([
+            'path' => $file_path,
+            'as' => $file_name,
+            'mime' => 'application/pdf',
+>>>>>>> 6eafc0a (up)
         ]);
     }
 }
