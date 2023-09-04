b71fa3ff079a45bb01f9b2b12b40dcea8ea8d590
Malebestia <vair81@gmail.com>
2023-09-04 13:13:31 +0200

    rebase

--- Resources/views/admin/home/acts/try_send_mail.blade.php
+++ Resources/views/admin/home/acts/try_send_mail.blade.php
@@ -3,6 +3,7 @@
     <x-col size="12">
             <form action="{!! Request::fullUrlWithQuery([]) !!}" method="POST">
                 @csrf
+<<<<<<< HEAD
 
                 <x-input.group type="text" name="subject" value="mail test" />
 
@@ -17,6 +18,14 @@
                 <x-input.group type="select" name="template" :options="$template_options"/>
 
                 <x-input.group type="select" name="mailer" :options="$mailer_options"/>
+=======
+                
+                <x-input.group type="text" name="from" />
+                
+                <x-input.group type="text" name="to" />
+
+                <x-input.group type="wysiwyg" name="body" />
+>>>>>>> e3e9786 (.)
 
                 <button class="btn btn-primary" type="submit">go!</button>
 
