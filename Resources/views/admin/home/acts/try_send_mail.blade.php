a40cc95e3d5d23b109ee71bf9b151b5dd985b451
Malebestia <vair81@gmail.com>
2023-09-04 13:14:53 +0200

    rebase

--- Resources/views/admin/home/acts/try_send_mail.blade.php
+++ Resources/views/admin/home/acts/try_send_mail.blade.php
@@ -20,16 +20,24 @@
                 <x-input.group type="select" name="mailer" :options="$mailer_options"/>
 =======
                 
-                <x-input.group type="text" name="subject" />
+                <x-input.group type="text" name="subject" value="mail test" />
 
-                <x-input.group type="text" name="from" />
+                <x-input.group type="text" name="from" value="staff@example.com" />
                 
+<<<<<<< HEAD
                 <x-input.group type="text" name="to" />
 
                 <x-input.group type="wysiwyg" name="body" />
 >>>>>>> e3e9786 (.)
+=======
+                <x-input.group type="text" name="to" value="marco.sottana@gmail.com" />
+>>>>>>> 7616aad (up)
 
+                <x-input.group type="text" name="body" value="invio mail di test" />
+                {{--
                 <x-input.group type="select" name="engine" :options="$options"/>
+                --}}
+                <x-input.group type="select" name="template" :options="$template_options"/>
 
                 <button class="btn btn-primary" type="submit">go!</button>
 
--- Resources/views/emails/templates/widgets/newfeatureEnd.blade.php
+++ Resources/views/emails/templates/widgets/newfeatureEnd.blade.php
@@ -10,7 +10,11 @@
 	</tr>
 </table>
 <<<<<<< HEAD
+<<<<<<< HEAD
 <img border="0" src="{{ Request::getSchemeAndHttpHost() }}/img/spacer.gif" width="1" height="15" class="divider"><br>
 =======
 <img border="0" src="{{ Request::getSchemeAndHttpHost() }}/vendor/beautymail/assets/images/widgets/spacer.gif" width="1" height="15" class="divider"><br>
 >>>>>>> 83f2b60 (up)
+=======
+<img border="0" src="{{ Request::getSchemeAndHttpHost() }}/img/spacer.gif" width="1" height="15" class="divider"><br>
+>>>>>>> 7616aad (up)
