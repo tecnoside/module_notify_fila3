cad98b1fc53c5bc90f1a67702ab8b1fde7e391e5
Malebestia <vair81@gmail.com>
2023-09-04 13:13:28 +0200

    rebase

--- Services/MailEngines/MailtrapEngine.php
+++ Services/MailEngines/MailtrapEngine.php
@@ -16,15 +16,21 @@ use Illuminate\Support\Facades\Mail;
 /**
  * Class SmsService.
  */
+<<<<<<< HEAD
 class MailtrapEngine
 {
     private static ?self $instance = null;
 
+=======
+class MailtrapEngine {
+    private static ?self $instance = null;
+>>>>>>> e8014ec (.)
     public ?string $from;
     public string $to;
     public string $driver;
     public ?string $body;
 
+<<<<<<< HEAD
     public function __construct()
     {
     }
@@ -33,11 +39,20 @@ class MailtrapEngine
     {
         if (null === self::$instance) {
             self::$instance = new self;
+=======
+    public function __construct() {
+    }
+
+    public static function getInstance(): self {
+        if (null === self::$instance) {
+            self::$instance = new self();
+>>>>>>> e8014ec (.)
         }
 
         return self::$instance;
     }
 
+<<<<<<< HEAD
     public static function make(): self
     {
         return static::getInstance();
@@ -45,6 +60,13 @@ class MailtrapEngine
 
     public function setLocalVars(array $vars): self
     {
+=======
+    public static function make(): self {
+        return static::getInstance();
+    }
+
+    public function setLocalVars(array $vars): self {
+>>>>>>> e8014ec (.)
         foreach ($vars as $k => $v) {
             $this->{$k} = $v;
         }
@@ -52,19 +74,28 @@ class MailtrapEngine
         return $this;
     }
 
+<<<<<<< HEAD
     public function send(): self
     {
+=======
+    public function send(): self {
+>>>>>>> e8014ec (.)
         dddx('WIP');
 
         return $this;
     }
 
+<<<<<<< HEAD
     public function try(): void
     {
+=======
+    public function try(): void {
+>>>>>>> e8014ec (.)
         // dddx($this);
 
         // Mail::raw('Hello World!', function($msg) {$msg->to('vair81@gmail.com')->subject('Test Email'); });
 
+<<<<<<< HEAD
         // try {
         Mail::raw((string) $this->body, function ($msg) {
             $msg->to($this->to)->subject('Test Email');
@@ -73,5 +104,14 @@ class MailtrapEngine
         // } catch (ErrorException $e) {
         //    throw new Exception('['.__LINE__.']['.__FILE__.']');
         // }
+=======
+        try {
+            Mail::raw($this->body, function ($msg) {
+                $msg->to($this->to)->subject('Test Email');
+            });
+        } catch (ErrorException $e) {
+            throw new Exception('['.__LINE__.']['.__FILE__.']');
+        }
+>>>>>>> e8014ec (.)
     }
 }
