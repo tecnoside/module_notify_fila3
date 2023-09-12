9ac3878cf1b7f1381993649863be79f41780ff0f
marco76tv <marco.sottana@gmail.com>
2023-09-12 17:18:41 +0200

    .

--- Providers/NotifyServiceProvider.php
+++ Providers/NotifyServiceProvider.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Modules\Notify\Providers;
 
-use Illuminate\Support\Facades\Notification;
+// use Illuminate\Support\Facades\Notification;
 use Modules\Xot\Providers\XotBaseServiceProvider;
 
 class NotifyServiceProvider extends XotBaseServiceProvider
