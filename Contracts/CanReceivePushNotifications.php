bad751f7d8381c852e9f9206b55d2c4bbd3e13de
Marco Guidolin <m.guidolin@egeatech.com>
2023-11-07 15:43:20 +0100

    ADD Firebase cloud messaging SDK integration and custom notification channel instance

---
+++ Contracts/CanReceivePushNotifications.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Modules\Notify\Contracts;
+
+use Illuminate\Support\Collection;
+
+interface CanReceivePushNotifications
+{
+    /**
+     * @return Collection<string>
+     */
+    public function getMobileDeviceTokens(): Collection;
+}
