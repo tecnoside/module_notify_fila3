ceea48f0e46ba7e3b5165c152e3e60144ea26f4d
marco76tv <marco.sottana@gmail.com>
2023-12-01 17:03:29 +0100

    Fix coding style and update exception class

--- Notifications/Channels/FirebaseCloudMessagingChannel.php
+++ Notifications/Channels/FirebaseCloudMessagingChannel.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Modules\Notify\Notifications\Channels;
 
-use Exception;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Log;
