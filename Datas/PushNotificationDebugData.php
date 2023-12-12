2962e921886e921ae002dbcafc0c8181a58157dd
marco76tv <marco.sottana@gmail.com>
2023-12-12 11:28:22 +0100

    rebase 1/72

--- Datas/PushNotificationDebugData.php
+++ Datas/PushNotificationDebugData.php
@@ -16,16 +16,28 @@ final class PushNotificationDebugData extends Data implements Arrayable
 {
     public function __construct(
         private readonly CanReceivePushNotifications $notifiable,
+<<<<<<< HEAD
         private readonly MobilePushNotification $notification,
         private readonly MulticastSendReport $sendReport,
+=======
+        private readonly MobilePushNotification      $notification,
+        private readonly MulticastSendReport         $sendReport,
+>>>>>>> b82a5c5 (ADD Firebase cloud messaging SDK integration and custom notification channel instance)
     ) {
     }
 
     public static function make(
         CanReceivePushNotifications $notifiable,
+<<<<<<< HEAD
         MobilePushNotification $notification,
         MulticastSendReport $sendReport,
     ): self {
+=======
+        MobilePushNotification      $notification,
+        MulticastSendReport         $sendReport,
+    ): self
+    {
+>>>>>>> b82a5c5 (ADD Firebase cloud messaging SDK integration and custom notification channel instance)
         return new self(
             $notifiable,
             $notification,
@@ -45,23 +57,40 @@ final class PushNotificationDebugData extends Data implements Arrayable
                 'successes' => $this->sendReport->successes()->count(),
                 'failures' => $this->sendReport->failures()->count(),
                 'successes_tokens' => $this->sendReport->successes()
+<<<<<<< HEAD
                     ->map(static fn (SendReport $report): array => [
+=======
+                    ->map(fn (SendReport $report) => [
+>>>>>>> b82a5c5 (ADD Firebase cloud messaging SDK integration and custom notification channel instance)
                         'type' => $report->target()->type(),
                         'value' => $report->target()->value(),
                     ]),
                 'failure_tokens' => $this->sendReport->failures()
+<<<<<<< HEAD
                     ->map(static fn (SendReport $report): array => [
+=======
+                    ->map(fn (SendReport $report) => [
+>>>>>>> b82a5c5 (ADD Firebase cloud messaging SDK integration and custom notification channel instance)
                         'type' => $report->target()->type(),
                         'value' => $report->target()->value(),
                     ]),
                 'unknown_tokens' => $this->sendReport
+<<<<<<< HEAD
                     ->filter(static fn (SendReport $report): bool => $report->messageWasSentToUnknownToken())
                     ->map(static fn (SendReport $report): array => [
+=======
+                    ->filter(fn (SendReport $report) => $report->messageWasSentToUnknownToken())
+                    ->map(fn (SendReport $report) => [
+>>>>>>> b82a5c5 (ADD Firebase cloud messaging SDK integration and custom notification channel instance)
                         'type' => $report->target()->type(),
                         'value' => $report->target()->value(),
                     ]),
                 'results' => $this->sendReport
+<<<<<<< HEAD
                     ->map(static fn (SendReport $report): array => [
+=======
+                    ->map(fn (SendReport $report) => [
+>>>>>>> b82a5c5 (ADD Firebase cloud messaging SDK integration and custom notification channel instance)
                         'target' => $report->target()->value(),
                         'result' => $report->result(),
                     ]),
