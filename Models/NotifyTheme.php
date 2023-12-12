fe1e532016767c0cd98b42bef6133bf98be4ab5b
marco76tv <marco.sottana@gmail.com>
2023-12-12 11:40:34 +0100

    Add sms_sent_at, sms_count, mail_sent_at, mail_count, token, first_name, and last_name properties to the Contact model. Remove language property from the Contact model. Remove body, body_html, theme, from_email, logo_src, logo_width, logo_height, and view_params properties from the NotifyTheme model

--- Models/NotifyTheme.php
+++ Models/NotifyTheme.php
@@ -12,6 +12,41 @@ use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 use Spatie\MediaLibrary\MediaCollections\Models\Collections\MediaCollection;
 
+<<<<<<< HEAD
+=======
+/**
+ * Modules\Notify\Models\NotifyTheme.
+ *
+ * @property int $id
+ * @property string|null $lang
+ * @property string|null $type
+ * @property string|null $subject
+ * @property string|null $body
+ * @property string|null $from
+ * @property Carbon|null $created_at
+ * @property string|null $created_by
+ * @property Carbon|null $updated_at
+ * @property string|null $updated_by
+ * @property string|null $post_type
+ * @property int|null $post_id
+ * @property string|null $body_html
+ * @property string|null $theme
+ * @property string|null $from_email
+ * @property string|null $logo_src
+ * @property int|null $logo_width
+ * @property int|null $logo_height
+ * @property array $view_params
+ * @property-read array $logo
+ * @property-read Model|\Eloquent $linkable
+ * @property-read MediaCollection<int, \Modules\Media\Models\Media> $media
+ * @property-read int|null $media_count
+ * @method static \Modules\Notify\Database\Factories\NotifyThemeFactory factory($count = null, $state = [])
+ * @method static Builder|NotifyTheme newModelQuery()
+ * @method static Builder|NotifyTheme newQuery()
+ * @method static Builder|NotifyTheme query()
+ * @mixin Eloquent
+ */
+>>>>>>> 07a2033 (Add sms_sent_at, sms_count, mail_sent_at, mail_count, token, first_name, and last_name properties to the Contact model. Remove language property from the Contact model. Remove body, body_html, theme, from_email, logo_src, logo_width, logo_height, and view_params properties from the NotifyTheme model)
 class NotifyTheme extends BaseModel implements HasMedia
 {
     use InteractsWithMedia;
