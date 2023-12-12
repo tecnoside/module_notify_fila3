f13e6781bb15086d8eba49db29a63f8ca8146f26
marco76tv <marco.sottana@gmail.com>
2023-12-12 11:36:02 +0100

    up: Updated Contact and NotifyTheme models

--- Models/Contact.php
+++ Models/Contact.php
@@ -101,81 +101,44 @@ use Illuminate\Support\Carbon;
 =======
 =======
  * @property int $id
+<<<<<<< HEAD
 >>>>>>> e268185 (up)
  * @property string|null $email
  * @property string|null $mobile_phone
+=======
+ * @property string $model_type
+ * @property string $model_id
+ * @property string|null $contact_type
+ * @property string|null $value
+ * @property string|null $user_id
+ * @property string|null $verified_at
+>>>>>>> 05798dc (up)
  * @property Carbon|null $created_at
  * @property Carbon|null $updated_at
- * @property string|null $created_by
  * @property string|null $updated_by
- * @property string|null $survey_pdf_id
- * @property string|null $attribute_1
- * @property string|null $attribute_2
- * @property string|null $attribute_3
- * @property string|null $attribute_4
+ * @property string|null $created_by
  * @property string|null $token
- * @property string|null $first_name
- * @property string|null $last_name
- * @property string|null $sms_sent_at
- * @property int|null $sms_count
- * @property string|null $mail_sent_at
- * @property int|null $mail_count
- * @property string|null $attribute_5
- * @property string|null $attribute_6
- * @property string|null $attribute_7
- * @property string|null $attribute_8
- * @property string|null $attribute_9
- * @property string|null $attribute_10
- * @property string|null $attribute_11
- * @property string|null $attribute_12
- * @property string|null $attribute_13
- * @property string|null $attribute_14
- * @property string|null $language
- * @property string|null $usesleft
- * @property string|null $sms_status_code
- * @property string|null $sms_status_txt
- * @property int|null $duplicate_count
- * @property int|null $order_column
  * @method static \Modules\Notify\Database\Factories\ContactFactory factory($count = null, $state = [])
  * @method static Builder|Contact newModelQuery()
  * @method static Builder|Contact newQuery()
  * @method static Builder|Contact query()
- * @method static Builder|Contact whereAttribute1($value)
- * @method static Builder|Contact whereAttribute10($value)
- * @method static Builder|Contact whereAttribute11($value)
- * @method static Builder|Contact whereAttribute12($value)
- * @method static Builder|Contact whereAttribute13($value)
- * @method static Builder|Contact whereAttribute14($value)
- * @method static Builder|Contact whereAttribute2($value)
- * @method static Builder|Contact whereAttribute3($value)
- * @method static Builder|Contact whereAttribute4($value)
- * @method static Builder|Contact whereAttribute5($value)
- * @method static Builder|Contact whereAttribute6($value)
- * @method static Builder|Contact whereAttribute7($value)
- * @method static Builder|Contact whereAttribute8($value)
- * @method static Builder|Contact whereAttribute9($value)
+ * @method static Builder|Contact whereContactType($value)
  * @method static Builder|Contact whereCreatedAt($value)
  * @method static Builder|Contact whereCreatedBy($value)
- * @method static Builder|Contact whereDuplicateCount($value)
- * @method static Builder|Contact whereEmail($value)
- * @method static Builder|Contact whereFirstName($value)
  * @method static Builder|Contact whereId($value)
- * @method static Builder|Contact whereLanguage($value)
- * @method static Builder|Contact whereLastName($value)
- * @method static Builder|Contact whereMailCount($value)
- * @method static Builder|Contact whereMailSentAt($value)
- * @method static Builder|Contact whereMobilePhone($value)
- * @method static Builder|Contact whereOrderColumn($value)
- * @method static Builder|Contact whereSmsCount($value)
- * @method static Builder|Contact whereSmsSentAt($value)
- * @method static Builder|Contact whereSmsStatusCode($value)
- * @method static Builder|Contact whereSmsStatusTxt($value)
- * @method static Builder|Contact whereSurveyPdfId($value)
+ * @method static Builder|Contact whereModelId($value)
+ * @method static Builder|Contact whereModelType($value)
  * @method static Builder|Contact whereToken($value)
  * @method static Builder|Contact whereUpdatedAt($value)
  * @method static Builder|Contact whereUpdatedBy($value)
+<<<<<<< HEAD
  * @method static Builder|Contact whereUsesleft($value)
 >>>>>>> 54bfe02 (.)
+=======
+ * @method static Builder|Contact whereUserId($value)
+ * @method static Builder|Contact whereValue($value)
+ * @method static Builder|Contact whereVerifiedAt($value)
+>>>>>>> 05798dc (up)
  * @mixin Eloquent
  *
  * @property string|null $email
