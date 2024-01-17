515dadb943c4fc0a4eb35861ab7429436a06060e
marco76tv <marco.sottana@gmail.com>
2024-01-17 16:12:24 +0100

    📝 docs(BuildMailMessageAction.php): fix PHPDoc comment formatting for $dataCollection parameter 🐛 fix(BuildMailMessageAction.php): instantiate MailMessage class with parentheses to fix syntax error 🐛 fix(MailtrapEngine.php): instantiate MailtrapEngine class with parentheses to fix syntax error 🐛 fix(MailService.php): instantiate MailService class with parentheses to fix syntax error 🐛 fix(SmsService.php): instantiate SmsService class with parentheses to fix syntax error

--- Services/MailEngines/MailtrapEngine.php
+++ Services/MailEngines/MailtrapEngine.php
@@ -32,7 +32,7 @@ class MailtrapEngine
     public static function getInstance(): self
     {
         if (! self::$instance instanceof MailtrapEngine) {
-            self::$instance = new self;
+            self::$instance = new self();
         }
 
         return self::$instance;
