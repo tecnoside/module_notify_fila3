d744177f5b20a4cb4b3bba6f51874f0c0d61acc3
marco76tv <marco.sottana@gmail.com>
2024-01-22 17:02:27 +0100

    🚧 chore(PdfTest.php): update namespace and add strict_types declaration for better code organization and type safety 🚧 chore(LinkableRelationManagerTest.php): update namespace and add strict_types declaration for better code organization and type safety

--- Tests/Unit/Actions/NotifyTheme/Attachment/PdfTest.php
+++ Tests/Unit/Actions/NotifyTheme/Attachment/PdfTest.php
@@ -1,6 +1,8 @@
 <?php
 
-namespace Modules\Notify\Actions\Tests\Unit\NotifyTheme\Attachment;
+declare(strict_types=1);
+
+namespace Modules\Notify\Tests\Unit\Actions\NotifyTheme\Attachment;
 
 use Modules\Notify\Actions\NotifyTheme\Attachment\Pdf;
 use Tests\TestCase;
@@ -14,20 +16,14 @@ final class PdfTest extends TestCase
 {
     private Pdf $pdf;
 
-    /**
-     * {@inheritdoc}
-     */
     protected function setUp(): void
     {
         parent::setUp();
 
-        /** @todo Correctly instantiate tested object to use it. */
+        /* @todo Correctly instantiate tested object to use it. */
         $this->pdf = new Pdf();
     }
 
-    /**
-     * {@inheritdoc}
-     */
     protected function tearDown(): void
     {
         parent::tearDown();
@@ -37,7 +33,7 @@ final class PdfTest extends TestCase
 
     public function testExecute(): void
     {
-        /** @todo This test is incomplete. */
+        /* @todo This test is incomplete. */
         self::markTestIncomplete();
     }
 }
--- Tests/Unit/Filament/Resources/NotifyThemeResource/RelationManagers/LinkableRelationManagerTest.php
+++ Tests/Unit/Filament/Resources/NotifyThemeResource/RelationManagers/LinkableRelationManagerTest.php
@@ -1,6 +1,8 @@
 <?php
 
-namespace Modules\Notify\Filament\Resources\Tests\Unit\NotifyThemeResource\RelationManagers;
+declare(strict_types=1);
+
+namespace Modules\Notify\Tests\Unit\Filament\Resources\NotifyThemeResource\RelationManagers;
 
 use Modules\Notify\Filament\Resources\NotifyThemeResource\RelationManagers\LinkableRelationManager;
 use Tests\TestCase;
@@ -14,20 +16,14 @@ final class LinkableRelationManagerTest extends TestCase
 {
     private LinkableRelationManager $linkableRelationManager;
 
-    /**
-     * {@inheritdoc}
-     */
     protected function setUp(): void
     {
         parent::setUp();
 
-        /** @todo Correctly instantiate tested object to use it. */
+        /* @todo Correctly instantiate tested object to use it. */
         $this->linkableRelationManager = new LinkableRelationManager();
     }
 
-    /**
-     * {@inheritdoc}
-     */
     protected function tearDown(): void
     {
         parent::tearDown();
@@ -37,13 +33,13 @@ final class LinkableRelationManagerTest extends TestCase
 
     public function testForm(): void
     {
-        /** @todo This test is incomplete. */
+        /* @todo This test is incomplete. */
         self::markTestIncomplete();
     }
 
     public function testTable(): void
     {
-        /** @todo This test is incomplete. */
+        /* @todo This test is incomplete. */
         self::markTestIncomplete();
     }
 }
