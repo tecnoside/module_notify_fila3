916557d88f872aca2033a88bee7d6735986fec5a
marco76tv <marco.sottana@gmail.com>
2024-02-28 18:08:08 +0100

    up

---
+++ Datas/FirebaseNotificationData.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Notify\Datas;
+
+use Spatie\LaravelData\Data;
+
+class FirebaseNotificationData extends Data
+{
+    public string $type; // ' => $data['type'],
+    public string $title; // ' => $data['title'],
+    public string $body; // ' => $data['body'],
+    public array $data; // ' => json_encode($data['data']),
+}
