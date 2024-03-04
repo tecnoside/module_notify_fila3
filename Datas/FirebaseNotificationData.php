ce3e6c4c03c1fef9d967d97137c7cad01c7a981d
Giuseppe <g.ialongo@egeatech.com>
2024-03-04 15:36:13 +0100

    UPDATE: fromType function

--- Datas/FirebaseNotificationData.php
+++ Datas/FirebaseNotificationData.php
@@ -15,4 +15,16 @@ class FirebaseNotificationData extends Data
     public string $body; // ' => $data['body'],
 
     public array $data; // ' => json_encode($data['data']),
+
+    public static function fromType(string $type): self
+    {
+        $data = [
+            'type' => $type,
+            'title' => trans('camping::notifications.'.strtoupper($type).'.title'),
+            'body' => trans('camping::notifications.'.strtoupper($type).'.body'),
+            'data' => [],
+        ];
+
+        return self::from($data);
+    }
 }
