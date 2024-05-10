cedd20005b0edf87a41db9c4e41a02b2f438d8bf
marco76tv <marco.sottana@gmail.com>
2024-05-10 12:36:44 +0200

    .

--- Database/Migrations/2020_01_01_000007_create_notify_themes_table.php
+++ Database/Migrations/2020_01_01_000007_create_notify_themes_table.php
@@ -19,7 +19,7 @@ class CreateNotifyThemesTable extends XotBaseMigration
     {
         // -- CREATE --
         $this->tableCreate(
-            static function (Blueprint $table): void {
+            function (Blueprint $table): void {
                 $table->increments('id');
                 $table->string('lang')->nullable();
                 $table->string('type')->nullable();
