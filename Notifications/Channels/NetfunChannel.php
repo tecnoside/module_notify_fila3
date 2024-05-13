17e0533282a6390fc515967b2ccb3669a929b563
Malebestia <vair81@gmail.com>
2024-05-13 12:40:50 +0200

    .

--- Notifications/Channels/NetfunChannel.php
+++ Notifications/Channels/NetfunChannel.php
@@ -7,7 +7,7 @@ namespace Modules\Notify\Notifications\Channels;
 use Modules\Notify\Actions\NetfunSendAction;
 use Modules\Notify\Contracts\CanThemeNotificationContract;
 use Modules\Notify\Notifications\ThemeNotification;
-use Modules\Xot\Contracts\ModelContactContract;
+// use Modules\Xot\Contracts\ModelContactContract;
 
 class NetfunChannel
 {
