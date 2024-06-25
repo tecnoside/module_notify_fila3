1f3c96f863d19b95242f78be79ce78f2acd1fdaa
marco <marco.sottana@gmail.com>
2024-06-25 10:17:49 +0200

    .

---
+++ Filament/Pages/SendEmail.php
@@ -0,0 +1,118 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Notify\Filament\Pages;
+
+use Filament\Actions\Action;
+use Filament\Facades\Filament;
+use Filament\Forms;
+use Filament\Forms\ComponentContainer;
+use Filament\Forms\Concerns\InteractsWithForms;
+use Filament\Forms\Contracts\HasForms;
+use Filament\Forms\Form;
+use Filament\Notifications\Notification;
+use Filament\Pages\Page;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Mail;
+use Modules\Notify\Datas\EmailData;
+use Modules\Notify\Emails\EmailDataEmail;
+use Modules\Xot\Filament\Traits\NavigationLabelTrait;
+
+/**
+ * @property ComponentContainer $emailForm
+ */
+class SendEmail extends Page implements HasForms
+{
+    use InteractsWithForms;
+    use NavigationLabelTrait;
+
+    public ?array $emailData = [];
+
+    protected static ?string $navigationIcon = 'heroicon-o-paper-airplane';
+
+    protected static string $view = 'notify::filament.pages.send-email';
+
+    public function mount(): void
+    {
+        $this->fillForms();
+    }
+
+    public function emailForm(Form $form): Form
+    {
+        return $form
+            ->schema(
+                [
+                    Forms\Components\Section::make()
+                        // ->description('Update your account\'s profile information and email address.')
+                        ->schema(
+                            [
+                                Forms\Components\TextInput::make('email_to')
+                                    // ->unique(ignoreRecord: true)
+                                    ->email()
+                                    ->required(),
+                                Forms\Components\TextInput::make('subject')
+                                    ->required(),
+                                Forms\Components\RichEditor::make('body')
+                                    ->required(),
+                            ]
+                        ),
+                ]
+            )
+            ->model($this->getUser())
+            ->statePath('emailData');
+    }
+
+    public function sendEmail(): void
+    {
+        $data = $this->emailForm->getState();
+        $email_data = EmailData::from($data);
+
+        Mail::to($data['email_to'])->send(
+            new EmailDataEmail($email_data)
+        );
+
+        Notification::make()
+            ->success()
+            // ->title(__('filament-panels::pages/auth/edit-profile.notifications.saved.title'))
+            ->title(__('check your email client'))
+            ->send();
+    }
+
+    protected function getForms(): array
+    {
+        return [
+            'emailForm',
+        ];
+    }
+
+    protected function getEmailFormActions(): array
+    {
+        return [
+            Action::make('emailFormActions')
+                // ->label(__('filament-panels::pages/auth/edit-profile.form.actions.save.label'))
+                ->label(__('send'))
+                ->submit('emailFormActions'),
+        ];
+    }
+
+    protected function getUser(): Authenticatable&Model
+    {
+        $user = Filament::auth()->user();
+
+        if (! $user instanceof Model) {
+            throw new \Exception('The authenticated user object must be an Eloquent model to allow the profile page to update it.');
+        }
+
+        return $user;
+    }
+
+    protected function fillForms(): void
+    {
+        // $data = $this->getUser()->attributesToArray();
+
+        // $this->editProfileForm->fill($data);
+        $this->emailForm->fill();
+    }
+}
