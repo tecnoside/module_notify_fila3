067a2e8d75ac6587a5aa7d1351de94955a5ed28e
marco <marco.sottana@gmail.com>
2024-07-01 17:32:44 +0200

    up

--- Models/NotifyThemeable.php
+++ Models/NotifyThemeable.php
@@ -18,7 +18,6 @@ use Illuminate\Support\Carbon;
  * @property Carbon|null $updated_at
  * @property string|null $updated_by
  * @property int|null $notify_theme_id
- *
  * @method static Builder|NotifyThemeable newModelQuery()
  * @method static Builder|NotifyThemeable newQuery()
  * @method static Builder|NotifyThemeable query()
@@ -30,7 +29,6 @@ use Illuminate\Support\Carbon;
  * @method static Builder|NotifyThemeable whereNotifyThemeId($value)
  * @method static Builder|NotifyThemeable whereUpdatedAt($value)
  * @method static Builder|NotifyThemeable whereUpdatedBy($value)
- *
  * @mixin Eloquent
  * @mixin \Eloquent
  */
