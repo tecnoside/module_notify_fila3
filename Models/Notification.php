435f0f742122f60ba72e6fe85ead8cb303c42021
marco <marco.sottana@gmail.com>
2024-07-02 17:13:08 +0200

    up

--- Models/Notification.php
+++ Models/Notification.php
@@ -89,6 +89,8 @@ use Illuminate\Support\Carbon;
  * @method static DatabaseNotificationCollection<int, static> get($columns = ['*'])
  * @method static DatabaseNotificationCollection<int, static> all($columns = ['*'])
  * @method static DatabaseNotificationCollection<int, static> get($columns = ['*'])
+ * @method static DatabaseNotificationCollection<int, static> all($columns = ['*'])
+ * @method static DatabaseNotificationCollection<int, static> get($columns = ['*'])
  * @mixin \Eloquent
  */
 class Notification extends DatabaseNotification
