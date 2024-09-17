9ffdb9844488450672935a6c11ed5d7b11ce2ee3
marco <marco.sottana@gmail.com>
2024-09-17 18:16:19 +0200

    up

--- Models/Notification.php
+++ Models/Notification.php
@@ -25,7 +25,6 @@ use Illuminate\Support\Carbon;
  * @property Carbon|null     $updated_at
  * @property string|null     $updated_by
  * @property Model|\Eloquent $notifiable
- *
  * @method static DatabaseNotificationCollection<int, static>        all($columns = ['*'])
  * @method static DatabaseNotificationCollection<int, static>        get($columns = ['*'])
  * @method static \Illuminate\Database\Eloquent\Builder|Notification newModelQuery()
@@ -190,9 +189,7 @@ use Illuminate\Support\Carbon;
  * @method static DatabaseNotificationCollection<int, static>        get($columns = ['*'])
  * @method static DatabaseNotificationCollection<int, static>        all($columns = ['*'])
  * @method static DatabaseNotificationCollection<int, static>        get($columns = ['*'])
- *
  * @mixin \Eloquent
- *
  * @method static DatabaseNotificationCollection<int, static> all($columns = ['*'])
  * @method static DatabaseNotificationCollection<int, static> get($columns = ['*'])
  * @method static DatabaseNotificationCollection<int, static> all($columns = ['*'])
@@ -217,7 +214,8 @@ use Illuminate\Support\Carbon;
  * @method static DatabaseNotificationCollection<int, static> get($columns = ['*'])
  * @method static DatabaseNotificationCollection<int, static> all($columns = ['*'])
  * @method static DatabaseNotificationCollection<int, static> get($columns = ['*'])
- *
+ * @method static DatabaseNotificationCollection<int, static> all($columns = ['*'])
+ * @method static DatabaseNotificationCollection<int, static> get($columns = ['*'])
  * @mixin Eloquent
  */
 class Notification extends DatabaseNotification
