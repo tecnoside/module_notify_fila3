580e961b33aad37d58229ec693710c3e4896f8d9
marco <marco.sottana@gmail.com>
2024-09-24 16:34:38 +0200

    up

--- Filament/Resources/ContactResource/Pages/ListContacts.php
+++ Filament/Resources/ContactResource/Pages/ListContacts.php
@@ -12,7 +12,7 @@ use Modules\UI\Filament\Actions\Table\TableLayoutToggleTableAction;
 
 class ListContacts extends ListRecords
 {
-    public TableLayoutEnum $layoutView = TableLayoutEnum::GRID;
+    public TableLayoutEnum $layoutView = TableLayoutEnum::LIST;
 
     protected static string $resource = ContactResource::class;
 
