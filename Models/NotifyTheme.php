b619fc66a3c37b8cffaf2926db1c8d280c21afce
marco76tv <marco.sottana@gmail.com>
2024-10-30 10:17:09 +0100

    .

--- Models/NotifyTheme.php
+++ Models/NotifyTheme.php
@@ -13,53 +13,53 @@ use Spatie\MediaLibrary\MediaCollections\Models\Collections\MediaCollection;
 /**
  * Modules\Notify\Models\NotifyTheme.
  *
- * @property int $id
- * @property string|null $lang
- * @property string|null $type
- * @property string|null $subject
- * @property string|null $body
- * @property string|null $from
- * @property Carbon|null $created_at
- * @property string|null $created_by
- * @property Carbon|null $updated_at
- * @property string|null $updated_by
- * @property string|null $post_type
- * @property int|null $post_id
- * @property string|null $body_html
- * @property string|null $theme
- * @property string|null $from_email
- * @property string|null $logo_src
- * @property int|null $logo_width
- * @property int|null $logo_height
- * @property array $view_params
- * @property array $logo
- * @property Model|Eloquent $linkable
+ * @property int                                               $id
+ * @property string|null                                       $lang
+ * @property string|null                                       $type
+ * @property string|null                                       $subject
+ * @property string|null                                       $body
+ * @property string|null                                       $from
+ * @property Carbon|null                                       $created_at
+ * @property string|null                                       $created_by
+ * @property Carbon|null                                       $updated_at
+ * @property string|null                                       $updated_by
+ * @property string|null                                       $post_type
+ * @property int|null                                          $post_id
+ * @property string|null                                       $body_html
+ * @property string|null                                       $theme
+ * @property string|null                                       $from_email
+ * @property string|null                                       $logo_src
+ * @property int|null                                          $logo_width
+ * @property int|null                                          $logo_height
+ * @property array                                             $view_params
+ * @property array                                             $logo
+ * @property Model|\Eloquent                                   $linkable
  * @property MediaCollection<int, \Modules\Media\Models\Media> $media
- * @property int|null $media_count
+ * @property int|null                                          $media_count
  *
  * @method static \Modules\Notify\Database\Factories\NotifyThemeFactory factory($count = null, $state = [])
- * @method static \Illuminate\Database\Eloquent\Builder|NotifyTheme newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|NotifyTheme newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|NotifyTheme query()
- * @method static \Illuminate\Database\Eloquent\Builder|NotifyTheme whereBody($value)
- * @method static \Illuminate\Database\Eloquent\Builder|NotifyTheme whereBodyHtml($value)
- * @method static \Illuminate\Database\Eloquent\Builder|NotifyTheme whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|NotifyTheme whereCreatedBy($value)
- * @method static \Illuminate\Database\Eloquent\Builder|NotifyTheme whereFrom($value)
- * @method static \Illuminate\Database\Eloquent\Builder|NotifyTheme whereFromEmail($value)
- * @method static \Illuminate\Database\Eloquent\Builder|NotifyTheme whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|NotifyTheme whereLang($value)
- * @method static \Illuminate\Database\Eloquent\Builder|NotifyTheme whereLogoHeight($value)
- * @method static \Illuminate\Database\Eloquent\Builder|NotifyTheme whereLogoSrc($value)
- * @method static \Illuminate\Database\Eloquent\Builder|NotifyTheme whereLogoWidth($value)
- * @method static \Illuminate\Database\Eloquent\Builder|NotifyTheme wherePostId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|NotifyTheme wherePostType($value)
- * @method static \Illuminate\Database\Eloquent\Builder|NotifyTheme whereSubject($value)
- * @method static \Illuminate\Database\Eloquent\Builder|NotifyTheme whereTheme($value)
- * @method static \Illuminate\Database\Eloquent\Builder|NotifyTheme whereType($value)
- * @method static \Illuminate\Database\Eloquent\Builder|NotifyTheme whereUpdatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|NotifyTheme whereUpdatedBy($value)
- * @method static \Illuminate\Database\Eloquent\Builder|NotifyTheme whereViewParams($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|NotifyTheme     newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|NotifyTheme     newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|NotifyTheme     query()
+ * @method static \Illuminate\Database\Eloquent\Builder|NotifyTheme     whereBody($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|NotifyTheme     whereBodyHtml($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|NotifyTheme     whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|NotifyTheme     whereCreatedBy($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|NotifyTheme     whereFrom($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|NotifyTheme     whereFromEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|NotifyTheme     whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|NotifyTheme     whereLang($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|NotifyTheme     whereLogoHeight($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|NotifyTheme     whereLogoSrc($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|NotifyTheme     whereLogoWidth($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|NotifyTheme     wherePostId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|NotifyTheme     wherePostType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|NotifyTheme     whereSubject($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|NotifyTheme     whereTheme($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|NotifyTheme     whereType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|NotifyTheme     whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|NotifyTheme     whereUpdatedBy($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|NotifyTheme     whereViewParams($value)
  *
  * @property \Modules\Xot\Contracts\ProfileContract|null $creator
  * @property \Modules\Xot\Contracts\ProfileContract|null $updater
